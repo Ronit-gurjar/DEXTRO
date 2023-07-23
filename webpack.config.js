b0fe14845ecf9c54d71a77b321301335b0a21ee0
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-23 19:13:45 +0530

    Fix: webpack config path

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
     module: {
         rules: [
             {
@@ -6,38 +8,12 @@ module.exports = {
                 test: /\.tsx?$/,
                 exclude: /node_modules/,
             },
-            {
-                test: /\.css$/i,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                        },
-                    },
-                    {
-                        loader: 'postcss-loader', // postcss loader needed for tailwindcss
-                        options: {
-                            postcssOptions: {
-                                ident: 'postcss',
-                                plugins: [tailwindcss, autoprefixer],
-                            },
-                        },
-                    },
-                ],
-            },
-            {
-                type: 'assets/resource',
-                test: /\.(png|jpg|jpeg|gif|woff|woff2|tff|eot|svg)$/,
-            },
         ]
     },
     resolve: {
         extensions: ['.tsx', '.js', '.ts']
     },
     output: {
-        filename: '[name].js',
-        path: path.join(__dirname, 'dist')
+        filename: 'index.js',
     }
 }
