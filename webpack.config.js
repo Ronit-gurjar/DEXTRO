7c9aa0f5eb7d0e632cd74e2a4722d0daf39f6d9c
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-23 18:46:33 +0530

    Add: webpack dependencies

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                use: 'ts-loader',
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1,
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader', // postcss loader needed for tailwindcss
+                        options: {
+                            postcssOptions: {
+                                ident: 'postcss',
+                                plugins: [tailwindcss, autoprefixer],
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                type: 'assets/resource',
+                test: /\.(png|jpg|jpeg|gif|woff|woff2|tff|eot|svg)$/,
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.js', '.ts']
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist')
+    }
+}
