8fed1b9f861af5f9f7c1ef9f1fd85a128342f3ff
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-27 22:55:17 +0530

    Files created by webpack build

--- dist/background.js
+++ dist/background.js
@@ -3,6 +3,10 @@ var __webpack_exports__ = {};
 /*!***********************************!*\
   !*** ./src/scripts/background.ts ***!
   \***********************************/
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extenstion successfully installed");
+});
 
 /******/ })()
 ;
+//# sourceMappingURL=background.js.map
