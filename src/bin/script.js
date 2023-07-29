ab9c562f70c7dc605fcdbc4b9e94a420cfe9617c
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-30 01:27:55 +0530

    Create: CLI script

---
+++ src/bin/script.js
@@ -0,0 +1,18 @@
+// Not to be Altered!
+
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
