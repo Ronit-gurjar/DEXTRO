2a7dc61441140ec0f85f4ebcdbeae2cde96aa3e0
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-30 02:03:46 +0530

    Fix: path

--- configs/tailwind.config.js
+++ configs/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{tsx,ts,jsx,js}',
+    '../src/**/*.{tsx,ts,jsx,js}',
   ],
   theme: {
     extend: {},
