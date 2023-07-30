77a4c365d924160a600080247093593844af4ed0
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-07-30 19:01:05 +0530

    Add: CLI script

---
+++ bin/script.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+const inquirer = require('inquirer');
+
+const QUESTIONS = [
+  {
+    type:"input",
+    message:"Project name:",
+    default:"Chrome-Ext-project",
+    name:"project_name"
+  },
+  {
+      type:"list",
+      message:"Would you like to use a framework?:",
+      name:"framework",
+      choices:["react","vanilla"]
+  },
+  {
+    type:"list",
+    message:"which variant would you prefer?:",
+    name:"variant",
+    choices:["Typescript","Javascript"]
+  },
+  {
+    type:"confirm",
+    message:"would you like to use tailwind css?:",
+    name:"tailwind"
+  }
+]
+
+
+
+inquirer
+  .prompt(QUESTIONS)
+  .then(answers => {
+    console.log(answers);
+  });
